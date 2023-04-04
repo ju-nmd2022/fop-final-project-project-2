2b97abadd97da5c83237cc41accd5a465e5ba4cb
jennelie01 <coje22ad@student.ju.se>
2023-04-04 13:11:16 +0200

    scenery

--- functions.js
+++ functions.js
@@ -1,4 +1,19 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+}
+
+function scenery() {
   background(110, 127, 128);
+  fill(192, 192, 192);
+  // floor
+  noStroke();
+  rect(0, 430, width, 110);
+  // windows
+  fill(0, 0, 0);
+  rect(130, 140, 115, 75, 20);
+  rect(430, 140, 115, 75, 20);
+}
+
+function draw() {
+  scenery();
 }
