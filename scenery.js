9b03cad0bf22be2ee5f080d5946c809ccd696874
jennelie01 <coje22ad@student.ju.se>
2023-04-13 13:39:50 +0200

    began scenery

---
+++ scenery.js
@@ -0,0 +1,12 @@
+let windowX = 100;
+let windowY = 100;
+
+background(110, 127, 128);
+fill(192, 192, 192);
+// floor
+noStroke();
+rect(0, 430, width, 200);
+// windows
+fill(0, 0, 0);
+rect(windowX + 30, windowY + 40, 115, 75, 20);
+rect(windowX + 330, windowY + 40, 115, 75, 20);
