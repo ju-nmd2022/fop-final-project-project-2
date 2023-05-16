919607375af630a0016a55b33816e85a2a917b21
gurkan-lite <117160581+gurkan-lite@users.noreply.github.com>
2023-05-16 21:24:35 +0200

    startscreens

--- startscreen.js
+++ startscreen.js
@@ -8,16 +8,17 @@ function setup() {
   frameRate(56);
 
   img = loadImage("./Spacebak.png");
+  img2 = loadImage("./space.jpg");
 
   // Create a button element
-  button = createButton('Start!');
+  button = createButton("Start!");
   // Set the position of the button
-  button.position(100, 200);
+  button.position(650, 650);
   // Add a callback function to handle button click event
   button.mousePressed(handleClick);
 
   // Add a CSS class to the button
-  button.addClass('custom-button');
+  button.addClass("custom-button");
 }
 
 function draw() {
@@ -30,20 +31,21 @@ function draw() {
 
 function drawStart() {
   background(0);
-  image(img, 10, 10, width, height);
+  image(img2, 10, 10, width, height);
   fill(254, 227, 132);
-  textSize(90);
+  textSize(80);
   textFont("times new roman");
-  text("Are YOU Ready For A New Adventure?", 230, 950);
+  textAlign(CENTER, CENTER); // Center align the text
+  text("Are YOU Ready For A New Adventure?", width / 2, 200);
   console.log("start");
 }
 
 function drawStartTwo() {
   background(0);
+  image(img, 10, 10, width, height);
   fill(254, 227, 132);
   textSize(90);
   textFont("times new roman");
-  text("A", 230, 950);
   button.hide(); // Hide the button when in the secondStart state
   // console.log("secondStart");
 }
@@ -54,29 +56,3 @@ function handleClick() {
     state = "secondStart";
   }
 }
-
-//   
-
-// //win, The following code was conducted from here: https://p5js.org/examples/image-load-and-display-image.html
-// function WinWin() {
-//   background(245, 250, 250);
-//   textSize(20);
-//   textFont("times new roman");
-//   text("YOU MADE IT!, You Saved the Groke!", 50, 50);
-//   console.log("win");
-//   state = "win";
-//   image(img, 40, 65);
-// }
-
-// // lost, The following code was conducted from here: https://p5js.org/examples/image-load-and-display-image.html
-// function LoseLose() {
-//   background(250, 50, 0, 30);
-//   textSize(25);
-//   textFont("times new roman");
-//   fill(255, 255, 255);
-//   text("That was not the plan! TRY AGAIN", 100, 100);
-//   console.log("lose");
-//   state = "lose";
-//   image(img1, 250, 165);
-// }
-
