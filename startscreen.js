931d7e195d98e2ae938d01a734858b54a2fdb94d
gurkan-lite <117160581+gurkan-lite@users.noreply.github.com>
2023-05-16 12:37:43 +0200

    button
    button working and changing screems

--- startscreen.js
+++ startscreen.js
@@ -1,26 +1,62 @@
 let isGameActive = true;
 let img;
-let gameState = "start";
 let state = "start";
+let button;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(56);
-}
 
-function preload() {
   img = loadImage("./Spacebak.png");
+
+  // Create a button element
+  button = createButton('Start!');
+  // Set the position of the button
+  button.position(100, 200);
+  // Add a callback function to handle button click event
+  button.mousePressed(handleClick);
+
+  // Add a CSS class to the button
+  button.addClass('custom-button');
 }
 
-function StartGame() {
-  image(img, -100, -100, width, height);
+function draw() {
+  if (state === "start") {
+    drawStart();
+  } else if (state === "secondStart") {
+    drawStartTwo();
+  }
+}
+
+function drawStart() {
+  background(0);
+  image(img, 10, 10, width, height);
   fill(254, 227, 132);
   textSize(90);
   textFont("times new roman");
-  text("Are YOU Ready For A New Adventure?",230,950);
-  
-  
+  text("Are YOU Ready For A New Adventure?", 230, 950);
+  console.log("start");
 }
+
+function drawStartTwo() {
+  background(0);
+  fill(254, 227, 132);
+  textSize(90);
+  textFont("times new roman");
+  text("A", 230, 950);
+  button.hide(); // Hide the button when in the secondStart state
+  // console.log("secondStart");
+}
+
+// Function to handle button click event
+function handleClick() {
+  if (state === "start") {
+    state = "secondStart";
+  }
+}
+
+//   
+
 // //win, The following code was conducted from here: https://p5js.org/examples/image-load-and-display-image.html
 // function WinWin() {
 //   background(245, 250, 250);
@@ -44,14 +80,3 @@ function StartGame() {
 //   image(img1, 250, 165);
 // }
 
-function draw() {
-  if (state === "start") {
-    StartGame();
-  } else if (state === "game") {
-    gamePlay();
-  } else if (state === "win") {
-    WinWin();
-  } else if (state === "lose") {
-    LoseLose();
-  }
-}
