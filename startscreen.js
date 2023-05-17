4c31f11c9c4c3236259152f22409480a3c41f636
gurkan-lite <117160581+gurkan-lite@users.noreply.github.com>
2023-05-17 11:58:06 +0200

    update

--- startscreen.js
+++ startscreen.js
@@ -34,20 +34,36 @@ function drawStart() {
   image(img2, 10, 10, width, height);
   fill(254, 227, 132);
   textSize(80);
-  textFont("times new roman");
+  textFont("Darumadrop One");
   textAlign(CENTER, CENTER); // Center align the text
-  text("Are YOU Ready For A New Adventure?", width / 2, 200);
+  text("Are YOU Ready For A New Adventure?", 750, 160);
   console.log("start");
 }
 
 function drawStartTwo() {
   background(0);
   image(img, 10, 10, width, height);
-  fill(254, 227, 132);
-  textSize(90);
-  textFont("times new roman");
-  button.hide(); // Hide the button when in the secondStart state
-  // console.log("secondStart");
+  fill(210, 43, 43);
+  textSize(150);
+  textFont("Darumadrop One");
+  text("URGENT!", 450, 190);
+  textSize(27);
+  textFont("Darumadrop One");
+  stroke(0, 0, 0);
+  fill(255, 255, 255);
+  strokeWeight(4);
+  text("NMD is stuck on the wrong spaceship full of impostors.", 435, 470);
+  text(
+    "Help NMD escape by collecting fuel and fly away in the JTH spaceship!",
+    530,
+    540
+  );
+  text(
+    "But be careful, the impostors are chasing you, and if you touch the boxes, they'll catch you!",
+    650,
+    590
+  );
+  button.hide();
 }
 
 // Function to handle button click event
